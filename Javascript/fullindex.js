2e8972e18264612a65477a456ead815f1dfd2c10
Abhi gautam <149761557+AbhiGautamcoder@users.noreply.github.com>
2024-07-20 18:28:33 +0530

    commit all

---
+++ Javascript/fullindex.js
@@ -0,0 +1,20 @@
+let firstName = 'Abhi'
+let lastName = 'Gautam'
+let age = 19
+let isHappy = true
+let userIntro = 'Hi I am'+firstName+''+lastName+age
+
+//this is the use memory creation phase and code execution phase 
+// console.log(firstName)
+//console log it is not working beause before del.. the function in a class
+var first = 'Abhi'
+let last = 'gautam'
+let age1 = 19
+const yearofBirth = 2005
+
+let userIntro1 = 'Hi I am'+first+last+age1+yearofBirth
+
+
+//console.log(typeof(typeof firstName))
+//output i 
+// string but why firstname is sring datatype
